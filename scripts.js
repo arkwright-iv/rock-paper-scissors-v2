0015821626c2be0601bc64ab2ee898110ea1281b
arkwright-iv <rb5558645@gmail.com>
2026-04-10 15:56:59 -0400

    added simple conditional logic to handle number to string play association

--- scripts.js
+++ scripts.js
@@ -7,8 +7,18 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-
+  let humanPlay = humanChoice.toLowerCase();
+  let computerPlay = computerChoice;
+  if (computerPlay === 0) {
+    computerPlay = "rock";
+  } else if (computerPlay === 1) {
+    computerPlay = "paper";
+  } else {
+    computerPlay = "scissors";
+  }
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
